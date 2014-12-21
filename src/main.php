917bab8a82838fdf29a791b82bfbf688bb785540
Nicholas Jordon <projectcleverweb@gmail.com>
2014-12-21 08:12:06 -0500

    initial commit

---
+++ src/main.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * PHP Autoload Library
+ * 
+ * @author    Nicholas Jordon
+ * @link      https://github.com/ProjectCleverWeb/PHP-Autoload
+ * @copyright 2014 Nicholas Jordon - All Rights Reserved
+ * @version   1.0.0
+ * @package   projectcleverweb\autoload\main
+ * @license   http://opensource.org/licenses/MIT
+ */
+
+namespace projectcleverweb\autoload;
+
+/**
+ * Main Class
+ */
+abstract class main {
+	
+}
---
+++ testing/tests/00_ConfigTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace projectcleverweb\autoload;
+
+/**
+ * @requires PHP 5.4
+ * @requires PHPUnit 4
+ */
+class ConfigTest extends Autoload_Testing_Config {
+	
+	/**
+	 * @test
+	 */
+	public function Placeholder() {
+		
+	}
+}
